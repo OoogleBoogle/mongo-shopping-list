d3bd9ab9d895a0f5c014a2e2a1925e8ac5418a4b
dengeist <eliasjmason@gmail.com>
2016-04-29 13:05:48 -0700

    Removes database-trawling tests.

--- test/test-server.js
+++ test/test-server.js
@@ -39,62 +39,48 @@ describe('Shopping List', function() {
             });
     });
 
-    // it('should add an item on POST', function(done) {
-    //     chai.request(app)
-    //         .post('/items')
-    //         .send({'name': 'Kale'})
-    //         .end(function(err, res) {
-    //             should.equal(err, null);
-    //             res.should.have.status(201);
-    //             res.should.be.json;
-    //             res.body.should.be.a('object');
-    //             res.body.should.have.property('name');
-    //             res.body.should.have.property('id');
-    //             res.body.name.should.be.a('string');
-    //             res.body.id.should.be.a('number');
-    //             res.body.name.should.equal('Kale');
-    //             storage.items.should.be.a('array');
-    //             storage.items.should.have.length(4);
-    //             storage.items[3].should.be.a('object');
-    //             storage.items[3].should.have.property('id');
-    //             storage.items[3].should.have.property('name');
-    //             storage.items[3].id.should.be.a('number');
-    //             storage.items[3].name.should.be.a('string');
-    //             storage.items[3].name.should.equal('Kale');
-    //             done();
-    //         });
-    // });
+    it('should add an item on POST', function(done) {
+        chai.request(app)
+            .post('/items')
+            .send({'name': 'Kale'})
+            .end(function(err, res) {
+                should.equal(err, null);
+                res.should.have.status(201);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.should.have.property('name');
+                res.body.should.have.property('_id');
+                res.body.name.should.be.a('string');
+                res.body._id.should.be.a('string');
+                res.body.name.should.equal('Kale');
+                done();
+            });
+    });
 
-    // it('should delete an item from storage', function(done) {
-    //     chai.request(app)
-    //         .delete('/items/1')
-    //         .end(function(err, res) {
-    //             should.equal(err, null);
-    //             res.should.have.status(201);
-    //             res.should.be.json;
-    //             res.body.should.be.a('array');
-    //             storage.items.should.have.length(3);
-    //             storage.items[1].name.should.not.equal('Tomatoes');
-    //             storage.items[1].id.should.not.equal('1');
-    //             done();
-    //         });
-    // });
+    it('should delete an item from storage', function(done) {
+        chai.request(app)
+            .delete('/items/1')
+            .end(function(err, res) {
+                should.equal(err, null);
+                res.should.have.status(201);
+                res.should.be.json;
+                res.body.should.be.a('array');
+                done();
+            });
+    });
 
-    // it('should modify an item from storage', function(done) {
-    //     chai.request(app)
-    //         .put('/items/2')
-    //         .send({'name': 'monads', 'id': 2})
-    //         .end(function(err, res) {
-    //             should.equal(err, null);
-    //             res.should.have.status(201);
-    //             res.should.be.json;
-    //             storage.items[1].name.should.equal('monads');
-    //             storage.items[1].id.should.equal(2);
-    //             res.body.should.be.a('array');
-    //             storage.items.should.have.length(3);
-    //             done();
-    //         });
-    // });
+    it('should modify an item from storage', function(done) {
+        chai.request(app)
+            .put('/items/2')
+            .send({'name': 'monads', '_id': 2})
+            .end(function(err, res) {
+                should.equal(err, null);
+                res.should.have.status(201);
+                res.should.be.json;
+                res.body.should.be.a('array');
+                done();
+            });
+    });
 
     after(function(done) {
         Item.remove(function() {
